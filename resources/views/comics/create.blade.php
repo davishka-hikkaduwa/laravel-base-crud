214a5058e9c7fdd21ff43a899a8dba2c04bb9085
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-11-09 15:50:04 +0100

    added css _create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,38 +5,40 @@
 @endsection
 
 @section('main')
-    <form method="POST" action="{{ route('comics.store') }}">
-        @csrf
-        <div>
-            <label for="title">Title:</label>
-            <input type="text" name="title">
-        </div>
-        <div>
-            <label for="description">Description:</label>
-            <textarea name="description" cols="30" rows="10"></textarea>
-        </div>
-        <div>
-            <label for="thumb">Thumb:</label>
-            <input type="text" name="thumb">
-        </div>
-        <div>
-            <label for="price">Price:</label>
-            <input type="text" name="price">
-        </div>
-        <div>
-            <label for="series">Series:</label>
-            <input type="text" name="series">
-        </div>
-        <div>
-            <label for="sale_date">Sale date:</label>
-            <input type="text" name="sale_date">
-        </div>
-        <div>
-            <label for="type">Type:</label>
-            <input type="text" name="type">
-        </div>
-        <div>
-            <input type="submit" value="Send">
-        </div>
-    </form>
+    <div class="container">
+        <form method="POST" action="{{ route('comics.store') }}">
+            @csrf
+            <div class="row">
+                <label for="title">Title:</label>
+                <input class="input" type="text" name="title">
+            </div>
+            <div class="row">
+                <label for="description">Description:</label>
+                <textarea class="input" name="description" cols="30" rows="10"></textarea>
+            </div>
+            <div class="row">
+                <label for="thumb">Thumb:</label>
+                <input class="input" type="text" name="thumb">
+            </div>
+            <div class="row">
+                <label for="price">Price:</label>
+                <input class="input" type="text" name="price">
+            </div>
+            <div class="row">
+                <label for="series">Series:</label>
+                <input class="input" type="text" name="series">
+            </div>
+            <div class="row">
+                <label for="sale_date">Sale date:</label>
+                <input class="input" type="text" name="sale_date">
+            </div>
+            <div class="row">
+                <label for="type">Type:</label>
+                <input class="input" type="text" name="type">
+            </div>
+            <div class="row">
+                <input class="input" type="submit" value="Send">
+            </div>
+        </form>
+    </div>
 @endsection
