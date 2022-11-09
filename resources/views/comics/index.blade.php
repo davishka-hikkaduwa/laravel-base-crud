ad29ce396225cacfb3c12bef9ed0fce9363949c0
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-11-09 10:26:05 +0100

    added function destroy

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -13,6 +13,11 @@
             @foreach ($comics as $comic)
                 <div>
                     <a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a>
+                    <form method="POST" action="{{ route('comics.destroy', $comic->id) }}">
+                        @csrf
+                        @method('DELETE')
+                        <input class="delete-btn" type="submit" value="Delete">
+                    </form>
                 </div>
             @endforeach
         </div>
